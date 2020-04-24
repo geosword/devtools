caf3ffb5681943d6a5547cc3963ca8c9f40c6819
Dylan Humphreys <dylan.humphreys@sectigo.com>
2020-04-24 22:08:35 +0100

    stand up an ansible role with molecule, and copy in your molecule scaffoled

--- mkansiblerole.sh
+++ mkansiblerole.sh
@@ -3,10 +3,13 @@ source /usr/bin/virtualenvwrapper.sh
 CWD=$(basename $PWD)
 # pseudo from here onwards
 mkvirtualenv $CWD
-pip install molecule docker
+pip install molecule docker boto boto3 botocore
 pip freeze > requriements.txt
 molecule init role $CWD
-mv $CWD/$CWD/*
-wget https://github.com/your_standard/gitignore
-wget https://github.com/your_standard/Dockerfile - could perhaps allow a command line argument to specify this
-wget https://github.com/your_standard/verify.yml
+## NOTE --template option of molecule is documented but not implemented in version 3.03
+mv $CWD/* ./
+curl -L https://github.com/geosword/molecule-scaffold/archive/master.zip --output master.zip
+[[ -f master.zip ]] && unzip master.zip && rm -r ./molecule/default
+[[ -d molecule-scaffold-master ]] && mv molecule-scaffold-master/molecule/default ./molecule/default && rm -r molecule-scaffold-master
+# cleanup
+rm master.zip
